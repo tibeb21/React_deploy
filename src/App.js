a5edc17be85acf2533bb12202d8a66a658c63411
tibeb21 <tibeb2018@gmail.com>
2022-07-31 01:53:22 -0400

    deploy my react app to githup pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import React from 'react'
+import myphoto from './photos/tibeb.jpg'
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> My First React App</h1>
+      <h3> Auther: Tibeb</h3>
+      <img src={myphoto} alt="profile-photos"  />
     </div>
   );
 }
